83a214c1da0be730433085770366c80615b27d6f
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-02-05 17:17:46 +0900

    43. JSONPlaceholder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import axios from "axios";
 import "./styles.css";
 
 export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=2")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>user1</button>
     </div>
   );
 }
